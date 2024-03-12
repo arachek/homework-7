b97926999252a2139fd78e69b123a6bd43e38c20
arachek <157759051+arachek@users.noreply.github.com>
2024-03-11 21:58:04 -0400

    Update index.php

--- public/index.php
+++ public/index.php
@@ -17,3 +17,28 @@ $url = $_SERVER["REQUEST_URI"];
 //if it is "/posts" return an array of posts via the post controller
 //if it is "/" return the homepage view from the main controller
 //if it is something else return a 404 view from the main controller
+
+$urlParts = explode('/', $url);
+
+
+$urlParts = array_values(array_filter($urlParts));
+
+
+$controllerName = ucfirst($urlParts[0]) . 'Controller';
+$methodName = $urlParts[1] ?? 'index';
+
+
+if (class_exists('app\\controllers\\' . $controllerName)) {
+    $controller = new $controllerName();
+
+    if (method_exists($controller, $methodName)) {
+        echo $controller->$methodName();
+    } else {
+        $mainController = new MainController();
+        echo $mainController->notFound();
+    }
+} else {
+    $mainController = new MainController();
+    echo $mainController->notFound();
+}
+?>
