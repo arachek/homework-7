fa17a1b0098644f7fb38f28fc67c4dd8d6c5f0a3
arachek <157759051+arachek@users.noreply.github.com>
2024-03-13 15:51:39 -0400

    Update index.php

--- public/index.php
+++ public/index.php
@@ -18,27 +18,4 @@ $url = $_SERVER["REQUEST_URI"];
 //if it is "/" return the homepage view from the main controller
 //if it is something else return a 404 view from the main controller
 
-$urlParts = explode('/', $url);
-
-
-$urlParts = array_values(array_filter($urlParts));
-
-
-$controllerName = ucfirst($urlParts[0]) . 'Controller';
-$methodName = $urlParts[1] ?? 'index';
-
-
-if (class_exists('app\\controllers\\' . $controllerName)) {
-    $controller = new $controllerName();
-
-    if (method_exists($controller, $methodName)) {
-        echo $controller->$methodName();
-    } else {
-        $mainController = new MainController();
-        echo $mainController->notFound();
-    }
-} else {
-    $mainController = new MainController();
-    echo $mainController->notFound();
-}
 ?>
